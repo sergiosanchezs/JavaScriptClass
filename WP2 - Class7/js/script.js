1509255909dae20a2145e3fbfd03c9e885d6a249
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-29 11:18:27 -0500

    class assignment done

--- WP2 - Class7/js/script.js
+++ WP2 - Class7/js/script.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    $("#myWidget").accordion({heightStyle: "content"});
+    $("#dialog1").hide();
+    $("#dialog2").hide();
+
+    $("#myWidgetAccordion").accordion({
+        heightStyle: "content"
+    });
+
+    $("#tabs").tabs({event: "mouseover"});
+
+    
+
+    $("#btnClickMe").click(function () {
+        $("#dialog1").dialog({
+            modal: true,
+            buttons: {
+                "OK": function() {
+                    $("#dialog2").dialog({
+                        modal: true,
+                        buttons: {
+                            Cancel: function() {
+                                $("#dialog2").dialog( "close" );
+                                }
+                        },
+                    });
+                },
+                Cancel: function() {
+                    $("#dialog1").dialog( "close" );
+                    }
+            },
+        });
+    });
 });
