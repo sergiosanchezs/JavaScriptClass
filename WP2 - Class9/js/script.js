5defd17d6fca5662a247eed13ae6c654c484ef8e
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-03-28 12:09:05 -0400

    class done

--- WP2 - Class9/js/script.js
+++ WP2 - Class9/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // alert("confirm jQuery is working");
-    // $("#faqs h1").fadeOut(1000).fadeIn(10000);
+    $("#faqs h1").fadeOut(1500).fadeIn(1500);
     // $("#faqs h2").fadeOut(5000).fadeIn(5000).fadeOut(5000).fadeIn(5000);
     $("#faqs h2 a").animate({
         fontSize:"400%", 
@@ -16,17 +16,4 @@ $(document).ready(function(){
                 5000,
                 "easeInBounce");
         });
-    
-    // $("#faqs h1").animate(
-    //     { 
-    //         fontSize:"800%",
-    //         opacity:.8 
-    //     },
-    //     10000, 
-    //     "easeOutBounce",
-    //     function()
-    //     {
-    //         $("h2 a").animate({fontSize:"200%",opacity:.5, color:"red" },5000, "easeOutBounce",function(){});
-            
-    //     });
 });
--- WP2 - Class9_2/js/script.js
+++ WP2 - Class9_2/js/script.js
@@ -1,32 +1,3 @@
 $(document).ready(function(){
-    // alert("confirm jQuery is working");
-    // $("#faqs h1").fadeOut(1000).fadeIn(10000);
-    // $("#faqs h2").fadeOut(5000).fadeIn(5000).fadeOut(5000).fadeIn(5000);
-    $("#faqs h2 a").animate({
-        fontSize:"400%", 
-        color:"blue", 
-        opacity:.8}, 
-        2000,
-        "easeOutBounce",
-        function(){
-            $("#faqs div").animate({
-                fontSize:"300%", 
-                color:"red", 
-                opacity:.6}, 
-                5000,
-                "easeInBounce");
-        });
     
-    // $("#faqs h1").animate(
-    //     { 
-    //         fontSize:"800%",
-    //         opacity:.8 
-    //     },
-    //     10000, 
-    //     "easeOutBounce",
-    //     function()
-    //     {
-    //         $("h2 a").animate({fontSize:"200%",opacity:.5, color:"red" },5000, "easeOutBounce",function(){});
-            
-    //     });
 });
