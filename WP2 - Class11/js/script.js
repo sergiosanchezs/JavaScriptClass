e60cfed95374321c7b884bddfbf252802109be69
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-04-11 08:48:44 -0400

    fancy box plugin done

--- WP2 - Class11/js/script.js
+++ WP2 - Class11/js/script.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
     // alert("confirm jQuery is working");
-    $("#faqs h1").fadeOut(1500).fadeIn(1500);
-    // $("#faqs h2").fadeOut(5000).fadeIn(5000).fadeOut(5000).fadeIn(5000);
-    $("#faqs h2 a").animate({
-        fontSize:"400%", 
-        color:"blue", 
-        opacity:.8}, 
-        2000,
-        "easeOutBounce",
-        function(){
-            $("#faqs div").animate({
-                fontSize:"300%", 
-                color:"red", 
-                opacity:.6}, 
-                5000,
-                "easeInBounce");
-        });
+    $("a#myimg1").fancybox();
 });
